aa3d9906407ad75e2c78d303f403187c74d669c2
liuluchao <J4BKwRSiCg-c>
2020-09-21 21:44:53 +0800

    项目创建

---
+++ app/src/main/java/com/smurf/app/SmurfApplication.java
@@ -0,0 +1,10 @@
+package com.smurf.app;
+
+import android.app.Application;
+
+public class SmurfApplication  extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
